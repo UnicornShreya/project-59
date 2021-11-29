165f69bc8ab751061c0ba5c742547e9778a74e65
UnicornShreya <91685021+UnicornShreya@users.noreply.github.com>
2021-11-29 10:58:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function flipimage() {
+    document.getElementById("image1").src = "https://media.wired.com/photos/5fd2c29c2a4ddaf3b6389694/191:100/w_1600,c_limit/Gear-Books-Roundup-671406905.jpg";
+}
